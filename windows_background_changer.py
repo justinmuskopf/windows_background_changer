a045498dfb1b94781a76a9e50d4fcd884db83c66
Justin Muskopf <Justin.Muskopf@RESPEC.com>
2019-05-24 17:21:20 -0500

    Support Python 2

--- windows_background_changer.py
+++ windows_background_changer.py
@@ -1,5 +1,5 @@
 import ctypes
-from sys import argv
+import sys
 from os.path import isfile
 
 class WindowsBackgroundChanger:
@@ -9,15 +9,19 @@ class WindowsBackgroundChanger:
         
         if not isfile(path_to_image):
             raise FileNotFoundError(path_to_image)
-        
-        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER,
-                                                   0,
-                                                   path_to_image,
-                                                   0)
+ 
+        if sys.version_info[0] < 3:
+            bg_change_function = ctypes.windll.user32.SystemParametersInfoA
+        else:
+            bg_change_function = ctypes.windll.user32.SystemParametersInfoW
+
+        bg_change_function(SPI_SETDESKWALLPAPER, 0, path_to_image, 0)
+
+
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("USAGE: {} PATH_TO_IMAGE".format(argv[0]))
+    if len(sys.argv) != 2:
+        print("USAGE: {} PATH_TO_IMAGE".format(sys.argv[0]))
         exit(1)
 
-    WindowsBackgroundChanger.change_background(argv[1])
+    WindowsBackgroundChanger.change_background(sys.argv[1])
